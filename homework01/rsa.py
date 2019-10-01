14efe010ff395a7a33bf0f8e5a2cc35a9a969499
Anna Golub <cinderella_ag@mail.ru>
2019-10-01 14:37:54 +0300

    Реализована функция is_prime(n)

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -17,7 +17,6 @@ def is_prime(n: int) -> bool:
             return False
         d += 1
     return True
-    pass
 
 
 def generate_keypair(p: int, q: int) -> Tuple[Tuple[int, int], Tuple[int, int]]:
@@ -27,20 +26,19 @@ def generate_keypair(p: int, q: int) -> Tuple[Tuple[int, int], Tuple[int, int]]:
         raise ValueError('p and q cannot be equal')
 
     # n = pq
-    n = p * q
+    # PUT YOUR CODE HERE
 
     # phi = (p-1)(q-1)
-    phi = (p - 1) * (q - 1)
+    # PUT YOUR CODE HERE
 
     # Choose an integer e such that e and phi(n) are coprime
     e = random.randrange(1, phi)
 
     # Use Euclid's Algorithm to verify that e and phi(n) are comprime
     g = gcd(e, phi)
-
     while g != 1:
         e = random.randrange(1, phi)
-    g = gcd(e, phi)
+        g = gcd(e, phi)
 
     # Use Extended Euclid's Algorithm to generate the private key
     d = multiplicative_inverse(e, phi)
@@ -56,12 +54,7 @@ def gcd(a: int, b: int) -> int:
     >>> gcd(3, 7)
     1
     """
-    while a * b != 0:
-        if a > b:
-            a %= b
-        else:
-            b %= a
-    return a + b
+    # PUT YOUR CODE HERE
     pass
 
 
@@ -70,31 +63,5 @@ def multiplicative_inverse(e: int, phi: int) -> int:
     >>> multiplicative_inverse(7, 40)
     23
     """
-    r0 = e % phi
-    a0 = 1
-    b0 = -(e // phi)
-
-    r1 = phi % r0
-    a1 = -(phi // r0)
-    b1 = 1 + (e // phi) * (phi // r0)
-    r2 = -1
-
-    if r1 == 0:
-        return r0
-
-    while r2 != 0:
-        a2 = a0 - a1 * (r0 // r1)
-        b2 = b0 - b1 * (r0 // r1)
-        r2 = a2 * e + b2 * phi
-        
-        r0 = r1
-        r1 = r2
-        a0 = a1
-        a1 = a2
-        b0 = b1
-        b1 = b2
-
-    if a0 < 0:
-        a0 += phi * (abs(a0) // phi + 1)
-    return a0
+    # PUT YOUR CODE HERE
     pass
